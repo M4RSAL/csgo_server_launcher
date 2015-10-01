12ec96403641bdeff0615e9d6cf213f002e52a1d
M4RSAL <m4rsal@gmail.com>
2015-10-01 15:47:01 +0200

    Update csgo_server.bat

--- csgo_server.bat
+++ csgo_server.bat
@@ -27,9 +27,10 @@ exit
 
 :update_server
 cls
-echo updating your csgo server...
+echo (%date%) - (%time%) csgo_server update started...
 steamcmd +login anonymous +force_install_dir c:\users\marcus-pc\desktop\csgo_server\SERVER_1 +app_update 740 validate
-echo DONE!
+echo (%date%) - (%time%) csgo_server update is done!
+timeout /t 3
 goto menu
 
 :start_server
@@ -42,16 +43,18 @@ echo 2. Start esl5on5 - COMP (128 tick)
 echo 2. Start Smoke server (64 tick)
 echo 5. Start Smoke server (128 tick)
 
-set server_cfg=
-set /p server_cfg=Make a Selection:
+set servercfg=
+set /p servercfg=Make a Selection:
 
 REM IF STATEMENTS
-IF %menu%==1 goto aim_map
-IF %menu%==2 goto aim_pistol
-IF %menu%==3 goto esl5on5
-IF %menu%==4 goto smoke_64
-IF %menu%==5 goto smoke_128
-IF NOT %menu%=="" goto start_server
+IF %servercfg%==1 goto aim_map
+IF %servercfg%==2 goto aim_pistol
+IF %servercfg%==3 goto esl5on5
+IF %servercfg%==4 goto smoke_64
+IF %servercfg%==5 goto smoke_128
+IF %servercfg%==q goto exit
+IF %servercfg%==b goto menu
+IF NOT %servercfg%"" goto start_server
 
 
 :aim_map
@@ -64,11 +67,11 @@ goto start1
 
 :start1
 echo (%date%) - (%time%) csgo_server started.
-echo (%date%) - (%time%) csgo_server started. >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) csgo_server started. >>csgo_server_logs/(%date%).txt 
 start /wait srcds.exe srcds -game csgo -console -usercon +game_type 0 +game_mode 1 -maxplayers_override 10 +exec aim_map.cfg -port 27015 -tickrate 128 +mapgroup mg_active +map de_dust2 -authkey E827ED43FD1235C8E57C314711235261
 
 echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting...
-echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>csgo_server_logs/(%date%).txt 
 goto start1
 
 :aim_pistol
@@ -81,11 +84,11 @@ goto start1
 
 :start2
 echo (%date%) - (%time%) csgo_server started.
-echo (%date%) - (%time%) csgo_server started. >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) csgo_server started. >>csgo_server_logs/(%date%).txt 
 start /wait srcds.exe srcds -game csgo -console -usercon +game_type 0 +game_mode 1 -maxplayers_override 10 +exec aim_pistol.cfg -port 27015 -tickrate 128 +mapgroup mg_active +map de_dust2 -authkey E827ED43FD1235C8E57C314711235261
 
 echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting...
-echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>csgo_server_logs/(%date%).txt 
 goto start2
 
 :esl5on5
@@ -98,11 +101,11 @@ goto start3
 
 :start3
 echo (%date%) - (%time%) csgo_server started.
-echo (%date%) - (%time%) csgo_server started. >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) csgo_server started. >>csgo_server_logs/(%date%).txt 
 start /wait srcds.exe srcds -game csgo -console -usercon +game_type 0 +game_mode 1 -maxplayers_override 10 +exec esl5on5.cfg -port 27015 -tickrate 128 +mapgroup mg_active +map de_dust2 -authkey E827ED43FD1235C8E57C314711235261
 
 echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting...
-echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>csgo_server_logs/(%date%).txt 
 goto start3
 
 :smoke_64
@@ -115,11 +118,11 @@ goto start3
 
 :start4
 echo (%date%) - (%time%) csgo_server started.
-echo (%date%) - (%time%) csgo_server started. >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) csgo_server started. >>csgo_server_logs/(%date%).txt 
 start /wait srcds.exe srcds -game csgo -console -usercon +game_type 0 +game_mode 1 -maxplayers_override 10 +exec smoke.cfg -port 27015 -tickrate 64 +mapgroup mg_active +map de_dust2 -authkey E827ED43FD1235C8E57C314711235261
 
 echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting...
-echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>csgo_server_logs/(%date%).txt 
 goto start4
 
 :smoke_128
@@ -132,11 +135,11 @@ goto start3
 
 :start5
 echo (%date%) - (%time%) csgo_server started.
-echo (%date%) - (%time%) csgo_server started. >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) csgo_server started. >>csgo_server_logs/(%date%).txt 
 start /wait srcds.exe srcds -game csgo -console -usercon +game_type 0 +game_mode 1 -maxplayers_override 10 +exec smoke.cfg -port 27015 -tickrate 128 +mapgroup mg_active +map de_dust2 -authkey E827ED43FD1235C8E57C314711235261
 
 echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting...
-echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>Watchdog_logs/(%date%).txt 
+echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>csgo_server_logs/(%date%).txt 
 goto start5
 
 
