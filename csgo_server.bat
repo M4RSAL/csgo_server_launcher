295c8231119b90cfb8a411ad61fc4b15a9aea4bc
M4RSAL <m4rsal@gmail.com>
2015-10-01 12:25:40 +0200

    Update csgo_server.bat

--- csgo_server.bat
+++ csgo_server.bat
@@ -14,7 +14,7 @@ echo depending on your language followed by Enter.
 :start
 echo (%date%) - (%time%) csgo_server started.
 echo (%date%) - (%time%) csgo_server started. >>Watchdog_logs/(%date%).txt 
-start /wait srcds.exe
+start /wait srcds.exe srcds -game csgo -console -usercon +game_type 0 +game_mode 1 -maxplayers_override 11 +exec esl5on5.cfg -port 27015 -tickrate 128 +mapgroup mg_active +map de_dust2 -authkey E827ED43FD1235C8E57C314711235261
 
 echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting...
 echo (%date%) - (%time%) WARNING: csgo_server crashed, restarting... >>Watchdog_logs/(%date%).txt 
